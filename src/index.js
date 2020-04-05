bc8069d26b3329cd0af95b9c67a551c1fd1117b3
Alvaro Jose P Peralta Ocampo <ajperalt@gmail.com>
2020-04-05 08:33:28 -0400

    actualizacion del curso domingo por la mañana

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react'
 import ReactDom from 'react-dom'
+import { App } from './App'
 
-
-ReactDom.render(<h1>'Hola platzi'</h1>,document.getElementById('app'));
+ReactDom.render(<App />, document.getElementById('app'))
